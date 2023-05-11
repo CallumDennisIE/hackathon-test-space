ac7cbb3499d45fa368d31d31824dc73dd3449571
CallumDennisIE <callumdennisie@gmail.com>
2023-05-11 12:17:19 +0000

    add ammo counter and ammo logic

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
+const targets = Array.from(document.getElementsByClassName('stormtrooper'));
+const blockers = Array.from(document.getElementsByClassName('supply-crate'));
+const body = document.getElementById('gameArea');
+const scoreArea = document.getElementById('scoreArea');
+const maxAmmoArea = document.getElementById('maxAmmoArea');
+const currentAmmoArea = document.getElementById('currentAmmoArea');
+
 enemy = {
     "destroyable": true
 };
@@ -6,47 +13,58 @@ cover = {
     "destroyable": false
 };
 
-
-
-const targets = Array.from(document.getElementsByClassName('stormtrooper'));
-const blockers = Array.from(document.getElementsByClassName('supply-crate'));
-const body = document.getElementById('gameArea');
-const scoreArea = document.getElementById('scoreArea');
-
 score = 0;
 scoreArea.innerHTML = score;
 
+maxAmmo = 6;
+currentAmmo = 6;
+maxAmmoArea.innerHTML = maxAmmo;
+currentAmmoArea.innerHTML = currentAmmo;
+
+
 targets.forEach(target => {
     target.addEventListener('click', function handleClick(event) {
-        if (enemy.destroyable) {
-            target.setAttribute('style', 'display: none;');
+        if (currentAmmo > 0) {
+            if (enemy.destroyable) {
+                target.setAttribute('style', 'display: none;');
+                score += 10;
+            }
+            else {
+                console.log("Hit Cover!");
+            }
 
-            score += 10;
-        }
-        else {
-            console.log("Hit Cover!");
+            scoreArea.innerHTML = score;
         }
 
-        scoreArea.innerHTML = score;
     });
 });
 
 blockers.forEach(blocker => {
     blocker.addEventListener('click', function handleClick(event) {
-        if (cover.destroyable) {
-            blocker.setAttribute('style', 'display: none;');
+        if (currentAmmo > 0) {
+            if (cover.destroyable) {
+                blocker.setAttribute('style', 'display: none;');
 
-            score += 10;
-        }
-        else {
-            console.log("Hit Cover!");
-        }
+                score += 10;
+            }
+            else {
+                console.log("Hit Cover!");
+            }
 
-        scoreArea.innerHTML = score;
+            scoreArea.innerHTML = score;
+        }
     });
 });
 
 body.addEventListener('click', function playSound(event) {
-    var audio = new Audio('Single_blaster_shot.mp3');
-    audio.play();
+    if (currentAmmo > 0) {
+        var audio = new Audio('Single_blaster_shot.mp3');
+        audio.play();
+        currentAmmo -= 1;
+        currentAmmoArea.innerHTML = currentAmmo;
+    } else {
+        var audio_out = new Audio('ammo-out.mp3');
+        audio_out.play();
+    }
+
 });
