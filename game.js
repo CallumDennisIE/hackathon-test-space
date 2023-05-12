eea122771e791f7f95a35aa7339f2b4c2e7c4177
CallumDennisIE <callumdennisie@gmail.com>
2023-05-12 13:11:50 +0000

    add some comments to js

--- game.js
+++ game.js
@@ -1,13 +1,16 @@
+// Define the area that the game will be contianed within
 const game = document.getElementById('gameArea');
 
+// Spawn the enemies
 createEnemy('center', 'moveLeft', 'sizeSml');
-
 createEnemy('left', 'moveRight', 'sizeBig');
-
 createEnemy('right', 'moveLeft', 'sizeMed');
 
+
+// Function to create enemies
 function createEnemy(position, moveDirection, spriteSize) {
 
+    // Set the size of the sprites based on the 3 sizes.
     if (spriteSize == "sizeSml") {
         size = 100;
     } else if (spriteSize == "sizeMed") {
@@ -16,23 +19,27 @@ function createEnemy(position, moveDirection, spriteSize) {
         size = 300;
     }
 
+    // Inverse the direction of movement animation if moving left
     if (moveDirection == "moveLeft") {
         movement = size * -1;
     } else {
         movement = size;
     }
 
-
+    // Create the div that will contain the enemy and cover
     enemyContainer = document.createElement('div');
     enemyContainer.classList.add('enemyContainer');
+    // Position the enemy container
     enemyContainer.classList.add(position);
     game.appendChild(enemyContainer);
 
+    // Create the span that will contain the enemy sprite image
     enemy = document.createElement('span');
     enemy.classList.add('enemy');
     enemy.classList.add(moveDirection);
     enemy.classList.add('animate');
 
+    // Set the correct animation based on the size and movement of the enemy
     if (spriteSize == 'sizeSml') {
         if (moveDirection == "moveLeft") {
             enemy.style.setProperty('animation-name', 'moveSmlLeft');
@@ -53,25 +60,23 @@ function createEnemy(position, moveDirection, spriteSize) {
         }
     }
 
-
-
-
     enemyContainer.appendChild(enemy);
 
+    // Create the image element that will show the enemy sprite
     enemySprite = document.createElement('img');
     enemySprite.setAttribute('src', 'StormTrooper.webp');
     enemySprite.style.setProperty('width', size.toString() + 'px');
     enemy.appendChild(enemySprite);
 
+    // Create the span that will contain the cover sprite image
     cover = document.createElement('span');
     cover.classList.add('cover');
     cover.style.setProperty('right', size.toString() + 'px');
     enemyContainer.appendChild(cover);
 
+    // Create the image element that will show the cover sprite
     coverSprite = document.createElement('img');
     coverSprite.setAttribute('src', 'rebel_supply_crate.webp');
     coverSprite.style.setProperty('width', size.toString() + 'px');
     cover.appendChild(coverSprite);
-
-    document.documentElement.style.setProperty('--size', size.toString() + 'px');
 }
